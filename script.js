01dcc162093abe93d673a946af41a4d29d46d6e6
jxdang1 <dangjennifer21@gmail.com>
2023-10-02 17:55:05 -0500

    added arrays for each character

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 // Assignment code here
+//added arrays for each type of characters
 
+var  lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = ["1","2","3","4","5","6","7","8","9"];
+var specialChars = ['"', " ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// varLetters (from starter code)
+
+// Assignment starter code:
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
